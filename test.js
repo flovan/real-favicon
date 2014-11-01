4b8d24941c5c5ad90dc5a551ba3f9ed10149564f
Hayden Bleasel <haydenbleasel@gmail.com>
2014-11-01 20:46:55 +1100

    Add Firefox icons and manifest.webapp test

--- test.js
+++ test.js
@@ -11,14 +11,24 @@ real_favicon({
   design: {
     // These options reflect the settings available in RealFaviconGenerator
     ios: {
-      picture_aspect: 'background_and_margin',
-      background_color: '#654321',
-      margin: 4
-    },
-    windows: {
-      picture_aspect: 'white_silhouette',
-      background_color: '#123456'
-    }
+			picture_aspect: "background_and_margin",
+			margin: "4",
+			background_color: "#123456"
+		},
+		windows: {
+			picture_aspect: "white_silhouette",
+			background_color: "#654321"
+		},
+		firefox_app: {
+			picture_aspect: "circle",
+			keep_picture_in_circle: "true",
+			circle_inner_margin: "5",
+			background_color: "#456789",
+			app_name: "My sample app",
+			app_description: "Yet another sample application",
+			developer_name: "Philippe Bernard",
+			developer_url: "http://stackoverflow.com/users/499917/philippe-b"
+		}
   },
   settings: {
     // 0 = no compression, 5 = maximum compression
